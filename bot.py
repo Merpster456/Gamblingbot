ca21ba19ecfef16d0704142bc3cf2de77390e635
Merpster <ryan.quirk6@protonmail.ch>
2021-02-10 23:16:10 -0500

    Added hourly, and daily function.

--- bot.py
+++ bot.py
@@ -37,11 +37,54 @@ class Gambling_bot:
         message_box.send_keys(".dep all")
         message_box.send_keys(Keys.RETURN)
 
+    def hourly(self):
+        driver = self.driver
+        driver.get("https://discord.com/channels/731310911792480256/808115279221358602/")
+        sleep(5)
+        message_box = driver.find_element_by_xpath("//div[@aria-label='Message #casino']")
+        message_box.send_keys(".hourly")
+        message_box.send_keys(Keys.RETURN)
+        sleep(1)
+        message_box.send_keys(".roulette all")
+        message_box.send_keys(Keys.RETURN)
+        sleep(1)
+        message_box.send_keys("green")
+        message_box.send_keys(Keys.RETURN)
+        sleep(1)
+        message_box.send_keys(".dep all")
+        message_box.send_keys(Keys.RETURN)
+
+    def daily(self):
+        driver = self.driver
+        driver.get("https://discord.com/channels/731310911792480256/808115279221358602/")
+        sleep(5)
+        message_box = driver.find_element_by_xpath("//div[@aria-label='Message #casino']")
+        message_box.send_keys(".daily")
+        message_box.send_keys(Keys.RETURN)
+        sleep(1)
+        message_box.send_keys(".crash all")
+        message_box.send_keys(Keys.RETURN)
+        sleep(1)
+        message_box.send_keys("bust")
+        message_box.send_keys(Keys.RETURN)
+        sleep(1)
+        message_box.send_keys(".dep all")
+        message_box.send_keys(Keys.RETURN)
+
 email = input("Email: ")
 pwd = getpass.getpass()
 
 bot = Gambling_bot(email, pwd)
 bot.initiate()
+i = 0
+
 while True:
-    sleep(605)
+    sleep(600)
     bot.work()
+    i =+ 1
+    if (i%6 == 0):
+        bot.hourly()
+    if (i%144 == 0):
+        bot.daily()
+        i = 0
+
